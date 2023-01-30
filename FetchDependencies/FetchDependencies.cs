67252734f8f402ade66c8214bd6a488faa4262ed
GooeyNyan <Sanjwinds@gmail.com>
2023-01-30 23:10:52 +0800

    feat: cn support

--- FetchDependencies/FetchDependencies.cs
+++ FetchDependencies/FetchDependencies.cs
@@ -1,40 +1,74 @@
 ﻿using System.IO.Compression;
+using System.Net;
 
-namespace FetchDependencies {
-    public class FetchDependencies {
+namespace FetchDependencies
+{
+    public class FetchDependencies
+    {
         private const string UserAgent =
             "IINACT";
 
         public string DependenciesDir { get; }
 
-        public FetchDependencies() {
+        public FetchDependencies()
+        {
             var assemblyDir = AppDomain.CurrentDomain.BaseDirectory;
             DependenciesDir = Path.Combine(assemblyDir, "external_dependencies");
         }
 
-        public async Task GetFfxivPlugin() {
-            Directory.CreateDirectory(DependenciesDir);
-            var pluginZipPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.zip");
-            var pluginPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.dll");
+        //public async Task GetFfxivPlugin() {
+        //    Directory.CreateDirectory(DependenciesDir);
+        //    var pluginZipPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.zip");
+        //    var pluginPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.dll");
 
-            if (!await NeedsUpdate(pluginPath))
-                return;
+        //    if (!await NeedsUpdate(pluginPath))
+        //        return;
 
-            if (!File.Exists(pluginZipPath))
-                await DownloadPlugin(pluginZipPath);
+        //    if (!File.Exists(pluginZipPath))
+        //        await DownloadPlugin(pluginZipPath);
 
-            ZipFile.ExtractToDirectory(pluginZipPath, DependenciesDir, overwriteFiles: true);
-            File.Delete(pluginZipPath);
+        //    ZipFile.ExtractToDirectory(pluginZipPath, DependenciesDir, overwriteFiles: true);
+        //    File.Delete(pluginZipPath);
 
-            var patcher = new Patcher(DependenciesDir);
-            patcher.MainPlugin();
-            patcher.LogFilePlugin();
-            patcher.MemoryPlugin();
+        //    var patcher = new Patcher(DependenciesDir);
+        //    patcher.MainPlugin();
+        //    patcher.LogFilePlugin();
+        //    patcher.MemoryPlugin();
+        //}
+
+        public async Task GetFfxivPlugin()
+        {
+            try
+            {
+                Directory.CreateDirectory(DependenciesDir);
+                //var pluginZipPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.zip");
+                var pluginPath = Path.Combine(DependenciesDir, "FFXIV_ACT_Plugin.dll");
+
+                if (!await NeedsUpdate(pluginPath))
+                    return;
+
+                if (!File.Exists(pluginPath))
+                    await DownloadPlugin(pluginPath);
+
+                // ZipFile.ExtractToDirectory(pluginZipPath, DependenciesDir, overwriteFiles: true);
+                //File.Delete(pluginPath);
+
+                var patcher = new Patcher(DependenciesDir);
+                patcher.MainPlugin();
+                patcher.LogFilePlugin();
+                patcher.MemoryPlugin();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
-        private static async Task<bool> NeedsUpdate(string dllPath) {
+        private static async Task<bool> NeedsUpdate(string dllPath)
+        {
             if (!File.Exists(dllPath)) return true;
-            try {
+            try
+            {
                 using var plugin = new TargetAssembly(dllPath);
                 var httpClient = new HttpClient();
                 httpClient.Timeout = TimeSpan.FromSeconds(2);
@@ -44,18 +78,48 @@ namespace FetchDependencies {
                 var remoteVersion = new Version(remoteVersionString);
                 return remoteVersion > plugin.Version;
             }
-            catch {
+            catch
+            {
                 return false;
             }
         }
 
-        private static async Task DownloadPlugin(string path) {
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
-            await using var downloadStream = await httpClient.GetStreamAsync("https://www.iinact.com/updater/download");
-            await using var zipFileStream = new FileStream(path, FileMode.Create);
-            await downloadStream.CopyToAsync(zipFileStream);
-            zipFileStream.Close();
+        //private static async Task DownloadPlugin(string path)
+        //{
+        //    var httpClient = new HttpClient();
+        //    httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+        //    await using var downloadStream = await httpClient.GetStreamAsync("https://www.iinact.com/updater/download");
+        //    await using var zipFileStream = new FileStream(path, FileMode.Create);
+        //    await downloadStream.CopyToAsync(zipFileStream);
+        //    zipFileStream.Close();
+        //}
+
+        private static async Task DownloadPlugin(string path)
+        {
+            string sourceFile = @"D:\Games\DieMoeAct\Plugins\FFXIV_ACT_Plugin.dll";
+            string destinationFile = path;
+            try
+            {
+                File.Copy(sourceFile, destinationFile, true);
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+
+            //string url = "";
+            //HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://github.com/TundraWork/FFXIV_ACT_Plugin_CN/releases/latest");
+            //req.Method = "HEAD";
+            //req.AllowAutoRedirect = false;  
+            //HttpWebResponse myResp = (HttpWebResponse)req.GetResponse();
+            //if (myResp.StatusCode == HttpStatusCode.Redirect)
+            //    url = myResp.GetResponseHeader("Location");
+            //var bcd = url.Split(@"/")[7];
+            //var httpClient = new HttpClient();
+            //await using var downloadStream = await httpClient.GetStreamAsync($"https://github.com/TundraWork/FFXIV_ACT_Plugin_CN/releases/download/{bcd}/FFXIV_ACT_Plugin.dll");
+            //await using var zipFileStream = new FileStream(path, FileMode.Create);
+            //await downloadStream.CopyToAsync(zipFileStream);
+            //zipFileStream.Close();
         }
     }
 }
